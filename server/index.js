3f1c97049d074767dc22fdcd6d6503923c9b030e
debGambit <debasishchakraborty.rock@gmail.com>
2024-01-30 17:09:38 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { Server } = require("socket.io");
+const { createServer } = require('http');
+const cors = require('cors');
+
+const router = require('./router');
+const {addUser, removeUser, getUser, getUsersInRoom } =  require('./users');
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+app.use(cors());
+app.use(router);
+
+io.on('connection', (socket) => {
+    socket.on('join', ({ name, room}, callback) => {
+        let { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.join(user.room);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback();
+
+        
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        let user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    })
+});
+
+server.listen(PORT, () => {
+    console.log(`Server has started on port ${PORT}`);
+});
